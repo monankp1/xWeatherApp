a40474332cac28199c5cca73acd11d5216cda483
monankp1 <monankp1@gmail.com>
2024-04-01 12:25:30 +0530

    initial commit

---
+++ src/DataCard.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+
+const DataCard = (prop) => {
+
+    const containerStyle = {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+    }
+
+    const cardStyle = {
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        border: '1px',
+        margin: '5px',
+        width: '10rem',
+        height: '8rem',
+        background: 'white',
+        borderRadius: '10px',
+        marginTop: '5rem',
+        marginRight: '3rem',
+
+    }
+
+  return (
+    <div style={containerStyle}>
+        <div style={cardStyle} className='weather-card'>
+            <p>{prop.name}</p>
+            <p>{prop.info}</p>
+        </div>
+    </div>
+    
+  )
+}
+
+export default DataCard
---
+++ src/Weather.jsx
@@ -0,0 +1,82 @@
+import React,{useState, useEffect} from 'react';
+import DataCard from './DataCard';
+import axios from 'axios';
+
+const Weather = () => {
+
+    const [isVisible, setIsVisible] = useState(true);
+    const [cityName, setCityName] = useState('');
+    const [isLoading, setIsLoading] = useState(false);
+    const [data, setData] = useState(null)
+
+    const fetchData = async(name) => {
+        try {
+            const response = await axios.get("https://api.weatherapi.com/v1/current.json", {
+                params : {
+                    Key: "dd0838cb30e54d69bb4140843232710",
+                    q: name
+                }
+            }); 
+            setData(response.data);
+            setIsVisible(true);
+            
+        } catch (error) {
+            alert("Failed to fetch weather data");
+            console.error("Error while fetching data",error);
+            setIsVisible(false);
+        } finally{
+            setIsLoading(false);
+        }
+    };
+
+    const handleSearch = () => {
+        setIsLoading(true);
+        setIsVisible(false);
+        fetchData(cityName)
+    };
+
+    const buttonStyle = {
+        backgroundColor:'MediumSeaGreen',
+        color:'white',  
+        fontSize:'1rem',
+        height: '2.5rem',
+        width: '5rem', 
+        margin:'5px',
+        border: '1px',
+        borderRadius: '5px',
+        marginTop: '8rem'
+    }
+
+    const searchBoxStyle = {
+        height: '2.5rem',
+        width: '15rem',
+        border: '1px',
+        borderRadius: '10px',
+
+
+    }
+
+    
+  return (
+    <div style={{background: "#D3FCF9", height: '100vh'}}>
+        <input onChange={(e) => setCityName(e.target.value)} placeholder='Enter city name' style={searchBoxStyle}/>
+        <button onClick={handleSearch} style={buttonStyle}>Search</button>
+        <br />
+
+        {isLoading? (<p>Loading data...</p>) : (
+            isVisible && data ? (
+                <div style={{display:'flex', justifyContent:'center'}}> 
+                    <DataCard info={`${data.current.temp_c}°C`} name='Temperature'/>
+                    <DataCard info={`${data.current.humidity}%`} name='Humidity'/>
+                    <DataCard info={data.current.condition.text} name='Condition'/>
+                    <DataCard info={`${data.current.wind_kph}kph`} name='Wind Speed'/>
+                </div>
+                
+            ) : ('')
+        )}
+        
+    </div>
+  )
+}
+
+export default Weather
